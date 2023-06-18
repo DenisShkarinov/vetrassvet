cad5ee1157b4f8fe96ef6159b415fbb83c369545
Denis <67820415+DenisShkarinov@users.noreply.github.com>
2023-06-18 12:12:48 +0300

    Vetrassvet

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,8 @@
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-export default function Layout ({ children }) {
+import {Props} from "@/utils/utils";
+
+export default function Layout ({ children }: Props) {
     return (
         <>
             <Header></Header>
