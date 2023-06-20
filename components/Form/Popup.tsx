0872930f891a64bfc871139cbeec3f4913ecebb2
Denis <67820415+DenisShkarinov@users.noreply.github.com>
2023-06-20 20:01:25 +0300

    Vetrassvet

--- components/Form/Popup.tsx
+++ components/Form/Popup.tsx
@@ -16,28 +16,34 @@ export default function MakeAnAppointment () {
             </div>
             <form action="/">
                 <div className="inputfield">
-                    <label htmlFor="username"></label>
-                    <input name='usernamae' type="text" placeholder='Ваше имя' required={true}/>
+                    <label htmlFor="username">Ваше имя:</label>
+                    <input id='username' minLength={2} type="text" placeholder='Иван' required={true}/>
                 </div>
+                {/*
+                    should make or find phone validator
+                */}
                 <div className="inputfield">
-                    <input type="tel" placeholder="(999) 999 99-99" required={true}/>
+                    <label htmlFor="phone">Телефон: </label>
+                    <input type="tel" maxLength={13} minLength={11} pattern="^[78]\d+$" id="phone" placeholder="8 (999) 999 99-99" required={true}/>
                 </div>
+
                 <div className="inputfield">
-                    <label htmlFor="animal">Вид животного</label>
+                    <label htmlFor="animal">Вид животного:</label>
                     <Select name="animal" />
                 </div>
                 <div className="inputfield">
-                    <input type="text" placeholder="Кличка" required={true}/>
+                    <label htmlFor="nameOfAnimal">Кличка питомца: </label>
+                    <input type="text" minLength={2} id="nameOfAnimal" placeholder="Кличка" required={true}/>
                 </div>
                 <div className="inputfield">
-                    <label htmlFor="specialist"></label>
+                    <label htmlFor="specialist">Специалист: </label>
                     <Select name='doctor'></Select>
                 </div>
-                <div className="inputfield">
+                <div className="inputfield column">
                     <label htmlFor="textarea">Опишите проблему:</label>
-                    <textarea name="textarea" id="textarea" cols={30} rows={10} required={true}></textarea>
+                    <textarea name="textarea" id="textarea" minLength={20} cols={20} rows={10} required={true} ></textarea>
                 </div>
-                <button onSubmit={(e:FormEvent) => handleSubmit (e) } type="submit">OK</button>
+                <button onSubmit={(e:FormEvent) => handleSubmit (e) } type="submit">записаться на прием</button>
             </form>
         </div>
     )
